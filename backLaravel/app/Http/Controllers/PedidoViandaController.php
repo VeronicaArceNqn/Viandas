3f8acf15926b70d56fd3fafef7eab17487d127cd
VeronicaArceNqn <veronica.arce@est.fi.uncoma.edu.ar>
2023-11-12 03:31:17 -0300

    Función PedidoViandaController-obtenerPedidoViandasPorFecha

--- backLaravel/app/Http/Controllers/PedidoViandaController.php
+++ backLaravel/app/Http/Controllers/PedidoViandaController.php
@@ -100,4 +100,34 @@ class PedidoViandaController extends Controller
         ];
         return response()->json($data);
     }
+
+    public function obtenerPedidoViandasPorFecha(Request $request)
+    {
+        // Valida que se haya proporcionado la fecha en la solicitud
+        $this->validate($request, [
+             'fecha' => 'required|date',
+         ], [
+            'fecha.required' => 'El campo fecha es obligatorio.',
+            'fecha.date' => 'El campo fecha debe ser una fecha válida en formato YYYY-MM-DD.',
+        ]);
+
+        // Obtén la fecha desde la solicitud
+        $fecha = $request->input('fecha');
+
+        // Obtén las PedidoViandas para la fecha específica
+        $pedidoViandas = PedidoVianda::whereDate('fechaEntrega', $fecha)->get();
+
+        // Verifica si hay PedidoViandas
+        if ($pedidoViandas->isEmpty()) {
+            // No hay PedidoViandas, devuelve un mensaje indicando que no hay resultados
+            return response()->json(['message' => 'No hay PedidoViandas para la fecha especificada'], 200);
+        }
+
+        // Hay PedidoViandas, devuelve las PedidoViandas en formato JSON
+        $data = [
+            'message' => 'Listado de PedidoViandas para la fecha especificada generado correctamente',
+            'pedido_viandas' => $pedidoViandas
+        ];
+        return response()->json($data);
+    }
 }
